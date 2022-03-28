57e550f8aa5accac1ea7d3047629bf685abe4ec0
aditya <vinchuaditya@gmail.com>
2022-03-28 22:38:41 +0530

    [Aditya]added: Employee Payroll service

---
+++ EmplyeePayrollService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmplyeePayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeOperation emp = new EmployeeOperation();
+            Employee employee = new Employee();
+            employee.Name = "Amit";
+            employee.BasicPay = 5000;
+            double result = emp.UpdateEmployee(employee);
+            Console.WriteLine(result);
+        }
+    }
+}
